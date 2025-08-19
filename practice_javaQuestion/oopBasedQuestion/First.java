0f8f7103ebb1712a149a554d403ec9622d58fed7
Utkarshpatg <utkarshpathak5202005@gmail.com>
2025-08-19 10:39:57 +0530

    done more question

---
+++ practice_javaQuestion/oopBasedQuestion/First.java
@@ -0,0 +1,16 @@
+package oopBasedQuestion;
+
+public class First {
+	static int num;
+	
+	static{
+		num = 50;
+		
+	}
+	
+	public static void main(String... args) {
+		First m = new First();
+		
+	}
+
+}
---
+++ practice_javaQuestion/oopBasedQuestion/Question9.java
@@ -0,0 +1,11 @@
+package oopBasedQuestion;
+
+public class Question9 {
+	String accountHolder;
+	double balance;
+	public static void main(String[] args) {
+		Question9 call = new Question9();
+		System.out.println(call.accountHolder+"   "+ call.balance);
+	}
+
+}
