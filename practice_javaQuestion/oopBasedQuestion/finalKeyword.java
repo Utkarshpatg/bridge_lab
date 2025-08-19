7dd4b81aba5d7465a941e2c4af24f390af591946
Utkarshpatg <utkarshpathak5202005@gmail.com>
2025-08-19 09:59:14 +0530

    more question

---
+++ practice_javaQuestion/oopBasedQuestion/finalKeyword.java
@@ -0,0 +1,10 @@
+package oopBasedQuestion;
+
+public class finalKeyword {
+    final int num = 10;
+
+    public static void main(String[] args) {
+//        num = 20;
+//        System.out.println(num);   --> final variable can not change once it assgined
+    }
+}
